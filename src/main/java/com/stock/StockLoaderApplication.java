292ce1000ab93f32cba6521dfc60227753a09f87
ditochilin <ditochilin@gmail.com>
2021-09-26 19:03:58 +0300

    second commit
    secnd commit

--- src/main/java/com/stock/StockLoaderApplication.java
+++ src/main/java/com/stock/StockLoaderApplication.java
@@ -4,18 +4,13 @@ import com.stock.service.StockLoaderService;
 import com.stock.service.StockLoaderServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
 
 @SpringBootApplication
 public class StockLoaderApplication {
